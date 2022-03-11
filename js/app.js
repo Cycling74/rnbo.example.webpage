b8d7a720fb6d61380b6c130c0df9b9874a16de09
Sam Tarakajian <sam@cycling74.com>
2022-03-11 18:30:55 -0500

    show the name fo the patcher

--- js/app.js
+++ js/app.js
@@ -31,6 +31,10 @@ async function setup() {
     // Connect the device to the web audio graph
     device.node.connect(outputNode);
 
+    // (Optional) Extract the name of the patcher from the description
+    if (patcher.desc.meta && patcher.desc.meta.filename)
+        document.getElementById("patcher-title").innerText = patcher.desc.meta?.filename;
+
     // (Optional) Automatically create sliders for the device parameters
     makeSliders(device);
 
@@ -182,7 +186,7 @@ function attachOutports(device) {
     device.messageEvent.subscribe((ev) => {
         // Message events have a tag as well as a payload
         console.log(`${ev.tag}: ${ev.payload}`);
-	});
+    });
 }
 
 function makeMIDIKeyboard(device) {
