b39664113ed2017004c24fc51ac6c22bb41b56db
Sam Tarakajian <sam@cycling74.com>
2022-03-11 15:43:04 -0500

    midi 'keyboard'

--- js/app.js
+++ js/app.js
@@ -34,6 +34,9 @@ async function setup() {
     // (Optional) Automatically create sliders for the device parameters
     makeSliders(device);
 
+    // (Optional) Connect MIDI inputs
+    makeMIDIKeyboard(device);
+
     document.body.onclick = () => {
         context.resume();
     }
@@ -130,6 +133,57 @@ function makeSliders(device) {
 
         // Add the slider element
         pdiv.appendChild(sliderContainer);
+    });
+}
+
+function makeMIDIKeyboard(device) {
+    let mdiv = document.getElementById("rnbo-clickable-keyboard");
+    if (device.numMIDIInputPorts === 0) return;
+
+    mdiv.removeChild(document.getElementById("no-midi-label"));
+
+    const midiNotes = [49, 52, 56, 63];
+    midiNotes.forEach(note => {
+        const key = document.createElement("div");
+        const label = document.createElement("p");
+        label.textContent = note;
+        key.appendChild(label);
+        key.addEventListener("pointerdown", () => {
+            let midiChannel = 0;
+
+            // Format a MIDI message paylaod, this constructs a MIDI on event
+            let noteOnMessage = [
+                144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
+                note, // MIDI Note
+                100 // MIDI Velocity
+            ];
+        
+            let noteOffMessage = [
+                128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
+                note, // MIDI Note
+                0 // MIDI Velocity
+            ];
+        
+            // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
+            // to the global namespace. This includes the TimeNow constant as well as
+            // the MIDIEvent constructor.
+            let midiPort = 0;
+            let noteDurationMs = 250;
+        
+            // When scheduling an even to occur in the future, use the current audio context time
+            // multiplied by 1000 (converting seconds to milliseconds) for now.
+            let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime, midiPort, noteOnMessage);
+            let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
+        
+            device.scheduleEvent(noteOnEvent);
+            device.scheduleEvent(noteOffEvent);
+
+            key.classList.add("clicked");
+        });
+
+        key.addEventListener("pointerup", () => key.classList.remove("clicked"));
+
+        mdiv.appendChild(key);
     });
 }
 
